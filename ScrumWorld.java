a482cfe17c48e319b99d7472de4c7626420efb80
aparnasivakumar <sk.aparna15@gmail.com>
2014-10-11 02:23:27 -0700

    Completed the Board

--- ScrumWorld.java
+++ ScrumWorld.java
@@ -16,7 +16,7 @@ public class ScrumWorld  extends World
     private Tools tool;
     public Peg1 peg;
     private Rolled r;
-    private int previous;
+    private int previous =0;
     public ScrumWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -27,12 +27,10 @@ public class ScrumWorld  extends World
 
     public void act ()
     {
-       
-       //peg.movePeg(scrumdie.getNum());
        r.setNum("Last Rolled: " + scrumdie.getNum());
-       if(scrumdie.getNum() < 5){
-           peg.movePeg(scrumdie.getNum());
-        }
+       previous = previous + scrumdie.getNum();
+       peg.movePeg(previous);
+           
     }
 
     /**
@@ -73,5 +71,17 @@ public class ScrumWorld  extends World
         mainBoard[4]= new Cell(new StBurndown(-25),new int[]{380,350});
         mainBoard[5]= new Cell(new StBurndown(50),new int[]{430,350});
         mainBoard[6]= new Cell(new StEndOfDays(),new int[]{530,350});
+        mainBoard[7]= new Cell(new StImpediment(),new int[]{630,300});
+        mainBoard[8]= new Cell(new StOpportunity(),new int[]{660,255});
+        mainBoard[9]= new Cell(new StTool(),new int[]{650,200});
+        mainBoard[10]= new Cell(new StEndOfDays(),new int[]{635,160});
+        mainBoard[11]= new Cell(new StImpediment(),new int[]{607,120});
+        mainBoard[12]= new Cell(new StOpportunity(),new int[]{555,97});
+        mainBoard[13]= new Cell(new StTool(),new int[]{510,100});
+        mainBoard[14]= new Cell(new StEndOfDays(),new int[]{460,120});
+        mainBoard[15]= new Cell(new StSprintReview(),new int[]{430,160});
+        mainBoard[16]= new Cell(new StImpediment(),new int[]{410,200});
+        mainBoard[17]= new Cell(new StOpportunity(),new int[]{414,255});
+        mainBoard[18]= new Cell(new StTool(),new int[]{430,300});
     }
 }
